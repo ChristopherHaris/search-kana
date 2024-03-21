717ec13a78c62852e075fd4b323113b62ea9a274
Christopher Haris <christoharis2005@gmail.com>
2024-03-21 17:18:26 +0700

    init : magic search

---
+++ src/lib/vectorize.ts
@@ -0,0 +1,13 @@
+import { openai } from "./openai";
+
+// export const vectorize = async (input: string): Promise<number[]> => {
+//   const embeddingResponse = await openai.embeddings.create({
+//     input,
+//     model: "text-embedding-3-small",
+//   });
+
+//   const vector = embeddingResponse.data[0].embedding;
+
+//   return vector;
+// };
+ // change to use open source model
